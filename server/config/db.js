8c4f5c1cafab01f4666d6bac27196fe552b6c2b8
Jeet Mane <manejeet2002@gmail.com>
2022-10-25 02:51:12 +0530

    login page added to main

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
-  const MONGO_URI="mongodb+srv://AlumConnect:RBcPTwbKpPwhMS0r@cluster0.ushdsiv.mongodb.net/?retryWrites=true&w=majority"
+  const MONGO_URI="mongodb+srv://jeetmane:jeetmane@cluster0.ifjd0t5.mongodb.net/?retryWrites=true&w=majority"
 
   try {
     const conn = await mongoose.connect(MONGO_URI, {
--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -32,17 +32,17 @@ const sendMessage = asyncHandler(async (req, res) => {
     content: content,
     chat: chatId,
   };
-
+  
   try {
     var message = await Message.create(newMessage);
 
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
     message = await User.populate(message, {
       path: "chat.users",
       select: "name pic email",
     });
-
+    
     await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
 
     res.json(message);
